138318f4e0152b7ea9bf91500691c4017711209f
igor.vieira4 <igor.p.r.vieira@gmail.com>
2022-08-19 13:31:35 -0300

    chore: add a page for our online view

---
+++ src/pages/OnLineCeremony/mock.ts
@@ -0,0 +1,52 @@
+const mock = [
+  {
+    url: '50_f2rpln.png',
+    alt: '50 reais',
+    description: 'Drinks para tarde',
+    text: 'Cota de R$ 50,00 reais',
+    banner: 'katherine-sousa-ln2R1wJ8TCM-unsplash_xzanbe.jpg',
+    pix: '00020126330014BR.GOV.BCB.PIX011170045007136520400005303986540550.005802BR5925Igor Pereira Ramos Vieira6009SAO PAULO61080540900062070503***6304BE3B'
+  },
+  {
+    url: '100_idt0b3.png',
+    alt: '100 reais',
+    description: 'Almoço em Recife',
+    text: 'Cota de R$ 100,00 reais',
+    banner: 'nerfee-mirandilla-UgKVWZuCxCk-unsplash_qkqzf7.jpg',
+    pix: '00020126330014BR.GOV.BCB.PIX0111700450071365204000053039865406100.005802BR5925Igor Pereira Ramos Vieira6009SAO PAULO61080540900062070503***6304E2DD'
+  },
+  {
+    url: '250_rhe1vs.png',
+    alt: '250 reais',
+    description: 'Passeios em Porto de Galinhas',
+    text: 'Cota de R$ 250,00 reais',
+    banner: 'sonia-nadales-ufEBXnMK4xs-unsplash_vj6zkc.jpg',
+    pix: '00020126330014BR.GOV.BCB.PIX0111700450071365204000053039865406250.005802BR5925Igor Pereira Ramos Vieira6009SAO PAULO61080540900062070503***63046518'
+  },
+  {
+    url: '500_ei1a4i.png',
+    alt: '500 reais',
+    description: 'Jantar a Luz de Velas',
+    text: 'Cota de R$ 500,00 reais',
+    banner: 'niklas-ohlrogge-Xee4i8HJxgI-unsplash_qgfful.jpg',
+    pix: '00020126330014BR.GOV.BCB.PIX0111700450071365204000053039865406500.005802BR5925Igor Pereira Ramos Vieira6009SAO PAULO61080540900062070503***6304B2F7'
+  },
+  {
+    url: '750_vd3qmk.png',
+    alt: '750 reais',
+    description: 'Pousada em Porto de Galinhas',
+    text: 'Cota de R$ 750,00 reais',
+    banner: 'alexander-kaunas-67-sOi7mVIk-unsplash_z3orum.jpg',
+    pix: '00020126330014BR.GOV.BCB.PIX0111700450071365204000053039865406750.005802BR5925Igor Pereira Ramos Vieira6009SAO PAULO61080540900062070503***6304A928'
+  },
+  {
+    url: '1000_uexd5t.png',
+    alt: '1000 reais',
+    description: 'Viagem para Recife',
+    text: 'Cota de R$ 1000,00 reais',
+    banner: 'avel-chuklanov-b1qpD3bhPOc-unsplash_velxwu.jpg',
+    pix: '00020126330014BR.GOV.BCB.PIX01117004500713652040000530398654071000.005802BR5925Igor Pereira Ramos Vieira6009SAO PAULO61080540900062160512NUN7pOqX3teW6304CE72'
+  },
+]
+
+export default mock
